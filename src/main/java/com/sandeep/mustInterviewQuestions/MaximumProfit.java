c71f4c5a95f6b9ccd8305d2bdbc9fe76089b9bcd
kodebetter <sandeeptiwari0012@gmail.com>
2021-09-27 15:22:06 +0530

    for maximum profit

--- src/main/java/com/sandeep/mustInterviewQuestions/MaximumProfit.java
+++ src/main/java/com/sandeep/mustInterviewQuestions/MaximumProfit.java
@@ -1,16 +1,67 @@
 package com.sandeep.mustInterviewQuestions;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MaximumProfit {
 
-    int initialInvestment = 0;
+    class Stock implements Comparable<Stock> {
+        public int purchasePrice;
+        public int sellingPrice;
+        public int profit;
+
+        public Stock(int purchasePrice, int s) {
+            this.purchasePrice = purchasePrice;
+            this.sellingPrice = s;
+            this.profit = this.sellingPrice - this.purchasePrice;
+        }
+
+        public String toString() {
+            return "" + this.profit;
+        }
+
+        public int compareTo(Stock b) {
+            if (this.profit < b.profit) {
+                return 1;
+            } else if (this.profit > b.profit) {
+                return -1;
+            }
+            return 0;
+        }
+    }
+
+    int initialSavings = 0;
     List<Integer> stocksPurchasePrice = null;
     List<Integer> stocksSellingPrice = null;
 
-    public MaximumProfit(int initialInvestment, List<Integer> stocksPurchasePrice, List<Integer> stocksSellingPrice){
-
+    public MaximumProfit(int initialInvestment, List<Integer> stocksPurchasePrice, List<Integer> stocksSellingPrice) {
+        this.initialSavings = initialInvestment;
+        this.stocksPurchasePrice = stocksPurchasePrice;
+        this.stocksSellingPrice = stocksSellingPrice;
     }
 
-    
+    public int getMaximumProfit() {
+        List<Stock> stocklist = new ArrayList<Stock>();
+        int profit = 0;
+        for (int i = 0; i < stocksPurchasePrice.size(); i++) {
+            stocklist.add(new Stock(stocksSellingPrice.get(i), stocksSellingPrice.get(i)));
+
+        }
+        Collections.sort(stocklist);
+        int i = 0;
+        while (initialSavings > 0 && i < stocklist.size()) {
+            int p = stocklist.get(i).purchasePrice;
+            int savingsafter = initialSavings - p;
+            if (stocklist.get(i).profit < 0 || savingsafter < 0) {
+                i++;
+                continue;
+            }
+            initialSavings = savingsafter;
+            profit = profit + stocklist.get(i).profit;
+            i++;
+        }
+        return profit;
+    }
+
 }
